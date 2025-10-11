066088025ce9ea681b277aab1c64862abb524c51
AkiraTomori <ThaiMinhHuy1407@gmail.com>
2025-10-11 19:17:56 +0700

    Completed Week01

--- Week01/05-23127164-23127379/app.js
+++ Week01/05-23127164-23127379/app.js
@@ -1,25 +1,38 @@
+// https://viblo.asia/p/nodejs-bai-12-static-file-tap-tin-tinh-trong-nodejs-1VgZvPO25Aw
+// Đường dẫn trên giải thích về express.static()
+// https://www.w3schools.com/nodejs/nodejs_path.asp
+// Đường dẫn trên giải thích về path
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path'; 
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const __dirname = new URL('.', import.meta.url).pathname;
 
-app.use(express.static(__dirname +'/public'));
+app.use(express.static('publics'));
 
 // Main route
-app.get('/', function(req, res) {
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname +'/views/index.html');
+});
+
+// Secondary route
+app.get('/about-my-team.html', function(req, res) {
     res.sendFile(__dirname + '/views/about-my-team.html');
 });
 
 // Member 1: route
-app.get('/', function(req, res) {
+app.get('/about-member1.html', function(req, res) {
     res.sendFile(__dirname + '/views/about-member1.html');
 })
+
 // Member 2: route
-app.get('/', function(req, res) {
+app.get('/about-member2.html', function(req, res) {
     res.sendFile(__dirname + '/views/about-member2.html');
 })
 
 app.listen(3000, function() {
-    console.log('Server is running on port https://localhost:3000')
+    console.log('Server is running on port http://localhost:3000')
 });
 
